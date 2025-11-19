e569112fcc3adf5cbdf311e1916cb5235bac60d8
JoshG83 <josh_goyette@gmail.com>
2025-11-19 12:00:32 -0500

    Second Commit

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
+import os
 from flask import Flask, render_template
+import sass
 
 app = Flask(__name__)
+
+def compile_scss_if_needed():
+    scss = os.path.join(app.root_path, 'static', 'css', 'formpage.scss')
+    css = os.path.join(app.root_path, 'static', 'css', 'formpage.css')
+    try:
+        if not os.path.exists(css) or os.path.getmtime(scss) > os.path.getmtime(css):
+            compiled = sass.compile(filename=scss, output_style='expanded')
+            with open(css, 'w', encoding='utf-8') as f:
+                f.write(compiled)
+    except Exception as e:
+        app.logger.error('SCSS compile error: %s', e)
+
+@app.before_request
+def maybe_compile_assets():
+    # Only compile automatically in debug mode (development)
+    if app.debug:
+        compile_scss_if_needed()
 
 @app.route('/')
 def home():
