cd777a9fe81219ef7f4df3d3612d87152151d90c
JoshG83 <josh_goyette@gmail.com>
2025-11-19 10:33:45 -0500

    First Commit

---
+++ Milestone 3.py
@@ -0,0 +1,10 @@
+# Milestone 3
+# Joshua Goyette, Nidhi Patel, Sanjedah Haque
+
+# Milestone 3 Final Program: Enhanced Scheduling System
+# Requirements:
+# 3 Total Entities: Employee, Manager, and Notification Center
+# 4 Processes: Handle Requests, Notify and Publish, Generate Schedule, Backup and Sync
+# What we may need to code this milestone: Django, Python, HTML, Flask, Java
+
+#
